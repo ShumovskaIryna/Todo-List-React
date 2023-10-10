786d131770be3836132780a0a35d32e1d7cc0171
Iryna Shumovska <shumovska_ia@groupbwt.com>
2023-10-10 21:38:44 +0300

    add Filter & TodoItems to page

---
+++ src/components/Filters.jsx
@@ -0,0 +1,19 @@
+import Filter from './Filter';
+const Filters = () => {
+  const filters = [
+    { id: 1, name: 'All' },
+    { id: 2, name: 'Active' },
+    { id: 3, name: 'Completed' },
+  ];
+  return (
+    <div>
+      <ul class="nav nav-tabs nav-fill mb-3" id="ex1" role="tablist">
+        {filters.map((el) => (
+          <Filter key={el.id} filterItem={el} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Filters;
---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import FullItem from './FullItem';
+import { FaTrash, FaPen } from 'react-icons/fa';
+
+const TodoItem = (props) => {
+  const [shouldShowItemDetails, toggleItemDetails] = useState(false);
+  return (
+    <>
+      <tr class="fw-normal">
+        <th class="align-middle">
+          <div class="checkbox">
+            <input
+              class="form-check-input me-2"
+              type="checkbox"
+              value=""
+              aria-label="..."
+              id="checkbox1"
+            />
+            <label for="checkbox1">
+              <span class="ms-2">Task #{props.todoItem.id}</span>
+            </label>
+          </div>
+        </th>
+        <td class="align-middle">
+          <span>{props.todoItem.name}</span>
+        </td>
+        <td class="align-middle">
+          <h6 class="mb-0">
+            <span class={`badge bg-${props.todoItem.bg}`}>
+              {props.todoItem.priority} priority
+            </span>
+          </h6>
+        </td>
+
+        <td class="align-middle">
+          <a href="#!" data-mdb-toggle="tooltip" title="Edit">
+            <FaPen
+              role="edit_button"
+              className="edit"
+              icon="fa-solid fa-pen"
+              style={{ color: '#38b3ff' }}
+              onClick={() => {
+                toggleItemDetails((current) => !current);
+              }}
+            />
+          </a>
+        </td>
+        <td class="align-middle">
+          <a href="#!" data-mdb-toggle="tooltip" title="Remove">
+            <FaTrash
+              role="delete_button"
+              className="delete"
+              class="fas fa-trash-alt fa-lg text-warning"
+            />
+          </a>
+        </td>
+      </tr>
+
+      {shouldShowItemDetails ? (
+        <FullItem
+          shouldShowProductDetails={shouldShowItemDetails}
+          toggleItemDetails={toggleItemDetails}
+          fullItem={props.todoItem}
+        />
+      ) : (
+        <></>
+      )}
+    </>
+  );
+};
+export default TodoItem;
